b38576b0458e9a281578b57f4735df27144c3658
Mateusz Pohl <mateusz@dimo.zone>
2025-02-28 14:46:38 +0100

    fingerprint payload test

--- pkg/lorawan/status_test.go
+++ pkg/lorawan/status_test.go
@@ -20,7 +20,7 @@ func TestConvertToCloudEvents(t *testing.T) {
 	}{
 		{
 			name:             "Status payload",
-			input:            []byte(`{"data":{"device":{"serial":"60d4af69-86e8-b790-02d3-c0a9dc4d6c8a","softwareVersion":"v1.0.0"},"timestamp":1732224181876,"vehicle":{"make":"MINI","model":"Countryman","signals":[{"name":"batteryVoltage","timestamp":1732224181876,"value":12.95}],"year":2018}},"signature":"0x67bdfbfce03ef7c6577a4a64de037db97d882ef158ee6d1b3adc96e0e58599b2508bb74f8780e102e0c50b7b30385ed6160aa8218c9793cb00fc8f8b355a966c1b","time":"2024-11-21T21:23:01.876617869Z","type":"com.dimo.device.status.v2","vehicleTokenId":1, "deviceTokenId": 2222}`),
+			input:            []byte(`{"data":{"header": 2,"device":{"serial":"60d4af69-86e8-b790-02d3-c0a9dc4d6c8a","softwareVersion":"v1.0.0"},"timestamp":1732224181876,"vehicle":{"make":"MINI","model":"Countryman","signals":[{"name":"batteryVoltage","timestamp":1732224181876,"value":12.95}],"year":2018}},"signature":"0x67bdfbfce03ef7c6577a4a64de037db97d882ef158ee6d1b3adc96e0e58599b2508bb74f8780e102e0c50b7b30385ed6160aa8218c9793cb00fc8f8b355a966c1b","time":"2024-11-21T21:23:01.876617869Z","type":"com.dimo.device.status.v2","vehicleTokenId":1, "deviceTokenId": 2222}`),
 			expectError:      false,
 			length:           2,
 			expectedSubject:  "did:nft:2:0x45fbCD3ef7361d156e8b16F5538AE36DEdf61Da8_1",
@@ -28,7 +28,7 @@ func TestConvertToCloudEvents(t *testing.T) {
 		},
 		{
 			name:             "Status payload with no vehicleTokenId",
-			input:            []byte(`{"data":{"device":{"softwareVersion":"v1.0.0"},"timestamp":1732224181876,"vehicle":{"make":"MINI","model":"Countryman","year":2018}},"time":"2024-11-21T21:23:01.876617869Z","type":"com.dimo.device.status.v2", "deviceTokenId": 2222}`),
+			input:            []byte(`{"data":{"header": 2,"device":{"softwareVersion":"v1.0.0"},"timestamp":1732224181876,"vehicle":{"make":"MINI","model":"Countryman","year":2018}},"time":"2024-11-21T21:23:01.876617869Z","type":"com.dimo.device.status.v2", "deviceTokenId": 2222}`),
 			expectError:      false,
 			length:           2,
 			expectedSubject:  "",
@@ -41,9 +41,9 @@ func TestConvertToCloudEvents(t *testing.T) {
 		},
 		{
 			name:             "Fingerprint payload",
-			input:            []byte(`{"subject":"0x1234567890abcdef1234567890abcdef12345678","time":"2023-10-31T12:34:56Z","type":"zone.dimo.aftermarket.device.fingerprint","vehicleTokenId":1, "deviceTokenId": 2222, "data":{"timestamp":1638316800000,"device":{"rpiUptimeSecs":3600,"batteryVoltage":12.6,"softwareVersion":"1.0.0","hwVersion":"v1","imei":"123456789012345","serial":"unit123"},"vin":"1HGCM82633A123456","protocol":"ISO9141","odometer":12345.67}}`),
+			input:            []byte(`{"data":{"device":{"id": "F4CE368CC0DF1D1D","name": "0x3216049F6D65A414E785D1012F70D8944AA1EC44","protocol": "lora_helium","nonce": 28},"timestamp": 1732224181876,"vehicle": {"make": "MINI","model": "Countryman","signals":[{"name": "batteryVoltage","timestamp": 1732224181876,"value": 12.95}],"year": 2018},"header": 1},"signature": "0xd085bac8eb4dbbc76f413c763e9910b3501e883437c658fe5ac33d1a5a380440392c2dc93b5dcb180e47e062784d746d1a6643d7419d8a6c76b8c39ef9ba12601c", "time": "2024-11-21T21:23:01.876617869Z", "type": "com.dimo.device.status.v2","vehicleTokenId": 1, "deviceTokenId": 2222}`),
 			expectError:      false,
-			length:           2,
+			length:           3,
 			expectedSubject:  "did:nft:2:0x45fbCD3ef7361d156e8b16F5538AE36DEdf61Da8_1",
 			expectedProducer: "did:nft:2:0x325b45949C833986bC98e98a49F3CA5C5c4643B5_2222",
 		},
