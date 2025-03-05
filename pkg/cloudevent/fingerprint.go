f84a439cc11dc3f7be70c4b084d85a12604a76c3
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-03-05 16:57:48 -0500

    Add base modules package
    * cloudevent alias

--- pkg/cloudevent/fingerprint.go
+++ pkg/cloudevent/fingerprint.go
@@ -1,9 +1,9 @@
 package cloudevent
 
-// Fingerprint represents a fingerprint message which holds a vehicle's VIN
+// Fingerprint represents a fingerprint message which holds a vehicle's VIN.
 type Fingerprint struct {
 	VIN string
 }
 
-// FingerprintEvent is a CloudEvent for a fingerprint message
-type FingerprintEvent CloudEvent[Fingerprint]
+// FingerprintEvent is a CloudEvent for a fingerprint message.
+type FingerprintEvent = CloudEvent[Fingerprint]
