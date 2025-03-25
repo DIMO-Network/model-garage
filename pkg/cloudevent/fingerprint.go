7873667537522dae2b702ec74531b18acc1c0b18
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-03-25 12:06:57 -0400

    deprecate cloudevent package

--- pkg/cloudevent/fingerprint.go
+++
@@ -1,9 +0,0 @@
-package cloudevent
-
-// Fingerprint represents a fingerprint message which holds a vehicle's VIN.
-type Fingerprint struct {
-	VIN string `json:"vin"`
-}
-
-// FingerprintEvent is a CloudEvent for a fingerprint message.
-type FingerprintEvent = CloudEvent[Fingerprint]
