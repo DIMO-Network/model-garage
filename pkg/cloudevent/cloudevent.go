2b687533a65d8819976680d728296e475c35e9fb
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-03-25 13:35:06 -0400

    Update deprecated comments for go docs

--- pkg/cloudevent/cloudevent.go
+++ pkg/cloudevent/cloudevent.go
@@ -1,4 +1,5 @@
 // Package cloudevent provides types for working with CloudEvents.
+//
 // Deprecated: This package has been moved to github.com/DIMO-Network/cloudevent.
 package cloudevent
 
@@ -8,65 +9,79 @@ import (
 
 const (
 	// TypeStatus is the event type for status updates.
+	//
 	// Deprecated: Use github.com/DIMO-Network/cloudevent.TypeStatus instead.
 	TypeStatus = cloudeventpkg.TypeStatus
 
 	// TypeFingerprint is the event type for fingerprint updates.
+	//
 	// Deprecated: Use github.com/DIMO-Network/cloudevent.TypeFingerprint instead.
 	TypeFingerprint = cloudeventpkg.TypeFingerprint
 
 	// TypeVerifableCredential is the event type for verifiable credentials.
+	//
 	// Deprecated: Use github.com/DIMO-Network/cloudevent.TypeVerifableCredential instead.
 	TypeVerifableCredential = cloudeventpkg.TypeVerifableCredential
 
 	// TypeUnknown is the event type for unknown events.
+	//
 	// Deprecated: Use github.com/DIMO-Network/cloudevent.TypeUnknown instead.
 	TypeUnknown = cloudeventpkg.TypeUnknown
 
 	// SpecVersion is the version of the CloudEvents spec.
+	//
 	// Deprecated: Use github.com/DIMO-Network/cloudevent.SpecVersion instead.
 	SpecVersion = cloudeventpkg.SpecVersion
 )
 
 // RawEvent is a cloudevent with a json.RawMessage data field.
+//
 // Deprecated: Use github.com/DIMO-Network/cloudevent.RawEvent instead.
 type RawEvent = cloudeventpkg.RawEvent
 
 // CloudEvent represents an event according to the CloudEvents spec.
 // To Add extra headers to the CloudEvent, add them to the Extras map.
 // See https://github.com/cloudevents/spec/blob/v1.0.2/cloudevents/spec.md
+//
 // Deprecated: Use github.com/DIMO-Network/cloudevent.CloudEvent instead.
 type CloudEvent[A any] = cloudeventpkg.CloudEvent[A]
 
 // CloudEventHeader contains the metadata for any CloudEvent.
 // To add extra headers to the CloudEvent, add them to the Extras map.
+//
 // Deprecated: Use github.com/DIMO-Network/cloudevent.CloudEventHeader instead.
 type CloudEventHeader = cloudeventpkg.CloudEventHeader
 
 // NFTDID is a Decentralized Identifier for NFTs.
+//
 // Deprecated: Use github.com/DIMO-Network/cloudevent.NFTDID instead.
 type NFTDID = cloudeventpkg.NFTDID
 
 // DecodeNFTDID decodes a DID string into a DID struct.
+//
 // Deprecated: Use github.com/DIMO-Network/cloudevent.DecodeNFTDID instead.
 func DecodeNFTDID(did string) (NFTDID, error) {
 	return cloudeventpkg.DecodeNFTDID(did)
 }
 
 // EthrDID is a Decentralized Identifier for an Ethereum contract.
+//
 // Deprecated: Use github.com/DIMO-Network/cloudevent.EthrDID instead.
 type EthrDID = cloudeventpkg.EthrDID
 
 // DecodeEthrDID decodes a Ethr DID string into a DID struct.
+//
 // Deprecated: Use github.com/DIMO-Network/cloudevent.DecodeEthrDID instead.
 func DecodeEthrDID(did string) (EthrDID, error) {
 	return cloudeventpkg.DecodeEthrDID(did)
 }
 
 // Fingerprint represents a fingerprint message which holds a vehicle's VIN.
+//
 // Deprecated: Use github.com/DIMO-Network/cloudevent.Fingerprint instead.
 type Fingerprint = cloudeventpkg.Fingerprint
 
 // FingerprintEvent is a CloudEvent for a fingerprint message.
+//
 // Deprecated: Use github.com/DIMO-Network/cloudevent.FingerprintEvent instead.
 type FingerprintEvent = cloudeventpkg.FingerprintEvent
