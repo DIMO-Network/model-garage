1be2874aa7e1d028aaaba61de54f3941ce54e8ae
Allyson-English <allyson.english@gmail.com>
2025-07-11 13:54:42 -0400

    tests

--- pkg/migrations/00003_event_table_migration.go
+++ pkg/migrations/00003_event_table_migration.go
@@ -46,7 +46,7 @@ func downEventTable(ctx context.Context, tx *sql.Tx) error {
 const createVehicleEventStmt = `
 CREATE TABLE IF NOT EXISTS event (
 	-- original cloud event headers
-	cloud_event_id String COMMENT 'Identifier for the cloudevent.',
+	cloud_event_id String COMMENT 'identifier for the cloudevent',
     subject String COMMENT 'identifies the entity the event pertains to',
     source String COMMENT 'the entity that identified and submitted the event (oracle)',
     producer String COMMENT 'the origin of the data used to determine the event',
@@ -55,6 +55,6 @@ CREATE TABLE IF NOT EXISTS event (
 	event_name String COMMENT 'name of the event indicated by the oracle transmitting it',
 	event_time DateTime64(6, 'UTC') COMMENT 'denotes time at which the event described occurred, transmitted by oracle',
 	event_duration String COMMENT 'optional event duration field transmitted by oracle',
-	event_metadata String COMMENT 'arbitrary JSON metadata provided by the user, containing additional event-related information.'
+	event_metadata String COMMENT 'arbitrary JSON metadata provided by the user, containing additional event-related information'
 ) ENGINE = ReplacingMergeTree
 ORDER BY (subject, event_time, event_name, source) SETTINGS index_granularity = 8192;`
