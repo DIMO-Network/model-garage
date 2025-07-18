ee5972642344629dbc20281bf7fea58bb3b50d78
Allyson English <58833140+Allyson-English@users.noreply.github.com>
2025-07-18 09:15:15 -0400

    ex-3739-create-events-table (#171)

---
+++ pkg/migrations/00003_event_table_migration.go
@@ -0,0 +1,60 @@
+// Package migrations provides the functionality for managing database migrations for the vss tables.
+package migrations
+
+import (
+	"context"
+	"database/sql"
+	"runtime"
+
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	_, filename, _, _ := runtime.Caller(0)
+	registerFunc := func() { goose.AddNamedMigrationContext(filename, upEventTable, downEventTable) }
+	registerFuncs = append(registerFuncs, registerFunc)
+}
+
+func upEventTable(ctx context.Context, tx *sql.Tx) error {
+	// This code is executed when the migration is applied.
+	upStatements := []string{
+		createVehicleEventStmt,
+	}
+	for _, upStatement := range upStatements {
+		_, err := tx.ExecContext(ctx, upStatement)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func downEventTable(ctx context.Context, tx *sql.Tx) error {
+	// This code is executed when the migration is rolled back.
+	downStatements := []string{
+		"DROP TABLE event",
+	}
+	for _, downStatement := range downStatements {
+		_, err := tx.ExecContext(ctx, downStatement)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+const createVehicleEventStmt = `
+CREATE TABLE IF NOT EXISTS event (
+	-- original cloud event headers
+	cloud_event_id String COMMENT 'identifier for the cloudevent',
+    subject String COMMENT 'identifies the entity the event pertains to',
+    source String COMMENT 'the entity that identified and submitted the event (oracle)',
+    producer String COMMENT 'the origin of the data used to determine the event',
+
+	-- event infos
+	event_name String COMMENT 'name of the event indicated by the oracle transmitting it',
+	event_time DateTime64(6, 'UTC') COMMENT 'denotes time at which the event described occurred, transmitted by oracle',
+	event_duration_ns Int64 COMMENT 'optional event duration in nanoseconds field transmitted by oracle',
+	event_metadata String COMMENT 'arbitrary JSON metadata provided by the user, containing additional event-related information'
+) ENGINE = ReplacingMergeTree
+ORDER BY (subject, event_time, event_name, source) SETTINGS index_granularity = 8192;`
--- pkg/migrations/migrations_test.go
+++ pkg/migrations/migrations_test.go
@@ -8,12 +8,13 @@ import (
 	"github.com/DIMO-Network/clickhouse-infra/pkg/connect/config"
 	"github.com/DIMO-Network/clickhouse-infra/pkg/container"
 	"github.com/DIMO-Network/model-garage/pkg/migrations"
+	"github.com/DIMO-Network/model-garage/pkg/occurrences"
 	"github.com/DIMO-Network/model-garage/pkg/vss"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestMigration(t *testing.T) {
+func TestSignalMigration(t *testing.T) {
 	ctx := context.Background()
 	chcontainer, err := container.CreateClickHouseContainer(ctx, config.Settings{})
 	require.NoError(t, err, "Failed to create clickhouse container")
@@ -42,6 +43,47 @@ func TestMigration(t *testing.T) {
 		{Name: vss.CloudEventIDCol, Type: "String", Comment: "Id of the Cloud Event that this signal was extracted from."},
 		{Name: vss.ValueNumberCol, Type: "Float64", Comment: "float64 value of the signal collected."},
 		{Name: vss.ValueStringCol, Type: "String", Comment: "string value of the signal collected."},
+	}
+
+	// Check if the actual columns match the expected columns
+	require.Equal(t, expectedColumns, columns, "Unexpected table columns")
+
+	// Close the DB connection
+	err = db.Close()
+	assert.NoError(t, err, "Failed to close DB connection")
+	err = conn.Close()
+	assert.NoError(t, err, "Failed to close clickhouse connection")
+}
+
+func TestEventMigration(t *testing.T) {
+	ctx := context.Background()
+	chcontainer, err := container.CreateClickHouseContainer(ctx, config.Settings{})
+	require.NoError(t, err, "Failed to create clickhouse container")
+
+	defer chcontainer.Terminate(ctx)
+
+	db, err := chcontainer.GetClickhouseAsDB()
+	require.NoError(t, err, "Failed to get clickhouse db")
+
+	err = migrations.RunGoose(ctx, []string{"up", "-v"}, db)
+	require.NoError(t, err, "Failed to run migration")
+
+	conn, err := chcontainer.GetClickHouseAsConn()
+	require.NoError(t, err, "Failed to get clickhouse connection")
+
+	// Iterate over the rows and check the column names
+	columns, err := connect.GetTableCols(ctx, conn, occurrences.TableName)
+	require.NoError(t, err, "Failed to get current columns")
+
+	expectedColumns := []connect.ColInfo{
+		{Name: occurrences.CloudEventIDCol, Type: "String", Comment: "identifier for the cloudevent"},
+		{Name: occurrences.SubjectCol, Type: "String", Comment: "identifies the entity the event pertains to"},
+		{Name: occurrences.SourceCol, Type: "String", Comment: "the entity that identified and submitted the event (oracle)"},
+		{Name: occurrences.ProducerCol, Type: "String", Comment: "the origin of the data used to determine the event"},
+		{Name: occurrences.EventNameCol, Type: "String", Comment: "name of the event indicated by the oracle transmitting it"},
+		{Name: occurrences.EventTimeCol, Type: "DateTime64(6, 'UTC')", Comment: "denotes time at which the event described occurred, transmitted by oracle"},
+		{Name: occurrences.EventDurationNSCol, Type: "Int64", Comment: "optional event duration in nanoseconds field transmitted by oracle"},
+		{Name: occurrences.EventMetaDataCol, Type: "String", Comment: "arbitrary JSON metadata provided by the user, containing additional event-related information"},
 	}
 
 	// Check if the actual columns match the expected columns
