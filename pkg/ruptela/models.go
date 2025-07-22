d21edf84f1d3ed1d8f55a80622964219b95a112d
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-07-22 14:36:56 -0400

    Update ruptela cloudevent header extraction (#178)
    * Update ruptela cloudevent header extraction

--- pkg/ruptela/models.go
+++ pkg/ruptela/models.go
@@ -2,12 +2,13 @@ package ruptela
 
 import (
 	"encoding/json"
+	"time"
 )
 
 type RuptelaEvent struct {
 	DS             string          `json:"ds"`
 	Signature      string          `json:"signature"`
-	Time           string          `json:"time"`
+	Time           time.Time       `json:"time"`
 	Data           json.RawMessage `json:"data"`
 	VehicleTokenID *uint32         `json:"vehicleTokenId"`
 	DeviceTokenID  *uint32         `json:"deviceTokenId"`
