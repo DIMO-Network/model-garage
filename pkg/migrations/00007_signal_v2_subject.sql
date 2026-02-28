5572b215dc075e3fd4d23d806f383364d4f919a3
Dylan Moreland <dylan@dimo.zone>
2026-02-28 16:35:18 -0500

    Try to do the views

--- pkg/migrations/00007_signal_v2_subject.sql
+++ pkg/migrations/00007_signal_v2_subject.sql
@@ -1,6 +1,15 @@
 -- +goose Up
-RENAME TABLE signal TO signal_backup;
 
+-- Drop these state change views first, since they reference signal.
+DROP VIEW IF EXISTS signal_state_changes_mv;
+DROP VIEW IF EXISTS signal_last_state_mv;
+
+-- Back up existing tables.
+RENAME TABLE signal TO signal_backup;
+RENAME TABLE signal_last_state TO signal_last_state_backup;
+RENAME TABLE signal_state_changes TO signal_state_changes_backup;
+
+-- Recreate signal with subject instead of token_id.
 CREATE TABLE IF NOT EXISTS signal
 (
     `subject` String COMMENT 'Subject of the signal, typically a W3C DID.',
@@ -22,7 +31,129 @@ PARTITION BY toYYYYMM(timestamp)
 ORDER BY (subject, timestamp, name)
 COMMENT 'Contains signals extracted from incoming CloudEvents. Most column names refer to CloudEvent concepts.';
 
+-- Recreate signal_last_state and the associated view, based on the new signal table.
+CREATE TABLE IF NOT EXISTS signal_last_state (
+  subject String,
+  signal_name LowCardinality(String),
+  last_timestamp DateTime64(6, 'UTC'),
+  last_value Float64,
+  last_source String DEFAULT '',
+  last_producer String DEFAULT '',
+  version UInt64 DEFAULT now64()
+)
+ENGINE = ReplacingMergeTree(version)
+ORDER BY (subject, signal_name);
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS signal_last_state_mv
+TO signal_last_state
+AS
+SELECT
+  subject,
+  name as signal_name,
+  timestamp as last_timestamp,
+  value_number as last_value,
+  source as last_source,
+  producer as last_producer,
+  now64() as version
+FROM signal
+WHERE name IN ('isIgnitionOn');
+
+-- Recreate signal_state_changes similarly.
+CREATE TABLE IF NOT EXISTS signal_state_changes (
+  subject String,
+  signal_name LowCardinality(String),
+  timestamp DateTime64(6, 'UTC'),
+  new_state Float64,
+  prev_state Float64,
+  time_since_prev_seconds Int32,
+  source LowCardinality(String),
+  producer LowCardinality(String),
+  cloud_event_id String DEFAULT '',
+  version UInt64 DEFAULT now64(),
+  INDEX idx_subject_name_ts (subject, signal_name, timestamp) TYPE minmax GRANULARITY 4
+)
+ENGINE = ReplacingMergeTree(version)
+ORDER BY (subject, signal_name, timestamp)
+PARTITION BY toYYYYMM(timestamp);
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS signal_state_changes_mv
+TO signal_state_changes
+AS
+SELECT
+  s.subject,
+  s.name as signal_name,
+  s.timestamp,
+  s.value_number as new_state,
+  ifNull(ls.last_value, -1) as prev_state,
+  dateDiff('second', ifNull(ls.last_timestamp, s.timestamp), s.timestamp) as time_since_prev_seconds,
+  s.source,
+  s.producer,
+  s.cloud_event_id,
+  now64() as version
+FROM signal s
+LEFT JOIN (
+  SELECT * FROM signal_last_state FINAL
+) ls ON
+  s.subject = ls.subject AND
+  s.name = ls.signal_name
+WHERE
+  s.name IN ('isIgnitionOn') AND
+  (ls.last_value IS NULL OR s.value_number != ls.last_value) AND
+  (ls.last_timestamp IS NULL OR s.timestamp > ls.last_timestamp);
+
 -- +goose Down
+
+-- Drop the new views and tables.
+DROP VIEW IF EXISTS signal_state_changes_mv;
+DROP VIEW IF EXISTS signal_last_state_mv;
+
+DROP TABLE IF EXISTS signal_state_changes;
+DROP TABLE IF EXISTS signal_last_state;
+
 DROP TABLE IF EXISTS signal;
 
+-- Restore backups.
 RENAME TABLE signal_backup TO signal;
+RENAME TABLE signal_last_state_backup TO signal_last_state;
+RENAME TABLE signal_state_changes_backup TO signal_state_changes;
+
+-- Recreate the original materialized views with token_id.
+CREATE MATERIALIZED VIEW IF NOT EXISTS signal_last_state_mv
+TO signal_last_state
+AS
+SELECT
+  token_id,
+  name as signal_name,
+  timestamp as last_timestamp,
+  value_number as last_value,
+  source as last_source,
+  producer as last_producer,
+  now64() as version
+FROM signal
+WHERE name IN ('isIgnitionOn');
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS signal_state_changes_mv
+TO signal_state_changes
+AS
+SELECT
+  s.token_id,
+  s.name as signal_name,
+  s.timestamp,
+  s.value_number as new_state,
+  ifNull(ls.last_value, -1) as prev_state,
+  dateDiff('second', ifNull(ls.last_timestamp, s.timestamp), s.timestamp) as time_since_prev_seconds,
+  s.source,
+  s.producer,
+  s.cloud_event_id,
+  now64() as version
+FROM signal s
+LEFT JOIN (
+  SELECT * FROM signal_last_state FINAL
+) ls ON
+  s.token_id = ls.token_id AND
+  s.name = ls.signal_name
+WHERE
+  s.name IN ('isIgnitionOn') AND
+  (ls.last_value IS NULL OR s.value_number != ls.last_value) AND
+  (ls.last_timestamp IS NULL OR s.timestamp > ls.last_timestamp);
+-- +goose StatementEnd
